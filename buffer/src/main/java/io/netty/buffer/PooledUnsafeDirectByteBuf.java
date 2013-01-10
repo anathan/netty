fd62b5306ad49898884e8d97bebf40f9e805b5fa
Trustin Lee <trustin@gmail.com>
2013-01-10 18:27:16 +0900

    Use sun.misc.Unsafe to access a direct buffer if possible

--- buffer/src/main/java/io/netty/buffer/PooledUnsafeDirectByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/PooledUnsafeDirectByteBuf.java
@@ -16,18 +16,79 @@
 
 package io.netty.buffer;
 
+import sun.misc.Unsafe;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ScatteringByteChannel;
 
-final class PooledDirectByteBuf extends PooledByteBuf<ByteBuffer> {
+final class PooledUnsafeDirectByteBuf extends PooledByteBuf<ByteBuffer> {
 
-    PooledDirectByteBuf(int maxCapacity) {
+    private static final Field ADDRESS_FIELD;
+    private static final Unsafe UNSAFE;
+    private static final boolean NATIVE_ORDER = ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN;
+
+    static {
+        Unsafe unsafe;
+        try {
+            Field singleoneInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
+            singleoneInstanceField.setAccessible(true);
+            unsafe = (Unsafe) singleoneInstanceField.get(null);
+        } catch (Throwable t) {
+            throw new Error(t);
+        }
+        UNSAFE = unsafe;
+
+        Field addressField;
+        try {
+            addressField = Buffer.class.getDeclaredField("address");
+            addressField.setAccessible(true);
+            if (addressField.getLong(ByteBuffer.allocate(1)) != 0) {
+                throw new Error("heap buffer address must be 0");
+            }
+            ByteBuffer directBuf = ByteBuffer.allocateDirect(1);
+            if (addressField.getLong(directBuf) == 0) {
+                throw new Error("direct buffer address must be non-zero");
+            }
+            UnpooledDirectByteBuf.freeDirect(directBuf);
+        } catch (Throwable t) {
+            throw new Error(t);
+        }
+        ADDRESS_FIELD = addressField;
+    }
+
+    private long memoryAddress;
+
+    PooledUnsafeDirectByteBuf(int maxCapacity) {
         super(maxCapacity);
+    }
+
+    @Override
+    void init(PoolChunk<ByteBuffer> chunk, long handle, int offset, int length, int maxLength) {
+        super.init(chunk, handle, offset, length, maxLength);
+        initiMemoryAddress();
+    }
+
+    @Override
+    void initUnpooled(PoolChunk<ByteBuffer> chunk, int length) {
+        super.initUnpooled(chunk, length);
+        initiMemoryAddress();
+    }
+
+    private void initiMemoryAddress() {
+        ByteBuffer memory = this.memory;
+        try {
+            memoryAddress = ADDRESS_FIELD.getLong(memory) + offset;
+        } catch (Exception e) {
+            throw new Error("failed to get the address of a direct buffer", e);
+        }
     }
 
     @Override
@@ -43,43 +104,50 @@ final class PooledDirectByteBuf extends PooledByteBuf<ByteBuffer> {
     @Override
     public byte getByte(int index) {
         checkIndex(index);
-        return memory.get(idx(index));
+        return UNSAFE.getByte(addr(index));
     }
 
     @Override
     public short getShort(int index) {
         checkIndex(index, 2);
-        return memory.getShort(idx(index));
+        if (NATIVE_ORDER) {
+            return UNSAFE.getShort(addr(index));
+        }
+        return Short.reverseBytes(UNSAFE.getShort(addr(index)));
     }
 
     @Override
     public int getUnsignedMedium(int index) {
         checkIndex(index, 3);
-        index = idx(index);
-        return (memory.get(index) & 0xff) << 16 | (memory.get(index + 1) & 0xff) << 8 | memory.get(index + 2) & 0xff;
+        long addr = addr(index);
+        return (UNSAFE.getByte(addr) & 0xff) << 16 |(UNSAFE.getByte(addr + 1) & 0xff) << 8 |
+                UNSAFE.getByte(addr + 2) & 0xff;
     }
 
     @Override
     public int getInt(int index) {
         checkIndex(index, 4);
-        return memory.getInt(idx(index));
+        if (NATIVE_ORDER) {
+            return UNSAFE.getInt(addr(index));
+        }
+        return Integer.reverseBytes(UNSAFE.getInt(addr(index)));
     }
 
     @Override
     public long getLong(int index) {
         checkIndex(index, 8);
-        return memory.getLong(idx(index));
+        if (NATIVE_ORDER) {
+            return UNSAFE.getLong(addr(index));
+        }
+        return Long.reverseBytes(UNSAFE.getLong(addr(index)));
     }
 
     @Override
     public ByteBuf getBytes(int index, ByteBuf dst, int dstIndex, int length) {
         checkIndex(index, length);
-        if (dst instanceof PooledDirectByteBuf) {
-            PooledDirectByteBuf bbdst = (PooledDirectByteBuf) dst;
-            ByteBuffer data = bbdst.internalNioBuffer();
-            dstIndex = bbdst.idx(dstIndex);
-            data.clear().position(dstIndex).limit(dstIndex + length);
-            getBytes(index, data);
+        if (dst instanceof PooledUnsafeDirectByteBuf) {
+            PooledUnsafeDirectByteBuf bbdst = (PooledUnsafeDirectByteBuf) dst;
+            UNSAFE.copyMemory(addr(index), bbdst.addr(dstIndex), length);
         } else if (dst.hasArray()) {
             getBytes(index, dst.array(), dst.arrayOffset() + dstIndex, length);
         } else {
@@ -140,50 +208,47 @@ final class PooledDirectByteBuf extends PooledByteBuf<ByteBuffer> {
     @Override
     public ByteBuf setByte(int index, int value) {
         checkIndex(index);
-        memory.put(idx(index), (byte) value);
+        UNSAFE.putByte(addr(index), (byte) value);
         return this;
     }
 
     @Override
     public ByteBuf setShort(int index, int value) {
         checkIndex(index, 2);
-        memory.putShort(idx(index), (short) value);
+        UNSAFE.putShort(addr(index), NATIVE_ORDER? (short) value : Short.reverseBytes((short) value));
         return this;
     }
 
     @Override
     public ByteBuf setMedium(int index, int value) {
         checkIndex(index, 3);
-        index = idx(index);
-        memory.put(index, (byte) (value >>> 16));
-        memory.put(index + 1, (byte) (value >>> 8));
-        memory.put(index + 2, (byte) value);
+        long addr = addr(index);
+        UNSAFE.putByte(addr, (byte) (value >>> 16));
+        UNSAFE.putByte(addr + 1, (byte) (value >>> 8));
+        UNSAFE.putByte(addr + 2, (byte) value);
         return this;
     }
 
     @Override
     public ByteBuf setInt(int index, int value) {
         checkIndex(index, 4);
-        memory.putInt(idx(index), value);
+        UNSAFE.putInt(addr(index), NATIVE_ORDER? value : Integer.reverseBytes(value));
         return this;
     }
 
     @Override
     public ByteBuf setLong(int index, long value) {
         checkIndex(index, 8);
-        memory.putLong(idx(index), value);
+        UNSAFE.putLong(addr(index), NATIVE_ORDER? value : Long.reverseBytes(value));
         return this;
     }
 
     @Override
     public ByteBuf setBytes(int index, ByteBuf src, int srcIndex, int length) {
         checkIndex(index, length);
-        if (src instanceof PooledDirectByteBuf) {
-            PooledDirectByteBuf bbsrc = (PooledDirectByteBuf) src;
-            ByteBuffer data = bbsrc.internalNioBuffer();
-            srcIndex = bbsrc.idx(srcIndex);
-            data.clear().position(srcIndex).limit(srcIndex + length);
-            setBytes(index, data);
+        if (src instanceof PooledUnsafeDirectByteBuf) {
+            PooledUnsafeDirectByteBuf bbsrc = (PooledUnsafeDirectByteBuf) src;
+            UNSAFE.copyMemory(bbsrc.addr(srcIndex), addr(index), length);
         } else if (src.hasArray()) {
             setBytes(index, src.array(), src.arrayOffset() + srcIndex, length);
         } else {
@@ -246,8 +311,9 @@ final class PooledDirectByteBuf extends PooledByteBuf<ByteBuffer> {
     @Override
     public ByteBuf copy(int index, int length) {
         checkIndex(index, length);
-        ByteBuf copy = alloc().directBuffer(capacity(), maxCapacity());
-        copy.writeBytes(this, index, length);
+        PooledUnsafeDirectByteBuf copy = (PooledUnsafeDirectByteBuf) alloc().directBuffer(capacity(), maxCapacity());
+        UNSAFE.copyMemory(addr(index), copy.addr(index), length);
+        copy.setIndex(index, index + length);
         return copy;
     }
 
@@ -281,5 +347,9 @@ final class PooledDirectByteBuf extends PooledByteBuf<ByteBuffer> {
     @Override
     public int arrayOffset() {
         throw new UnsupportedOperationException("direct buffer");
+    }
+
+    private long addr(int index) {
+        return memoryAddress + index;
     }
 }
